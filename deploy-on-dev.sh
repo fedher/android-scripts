a219670a1ef1772eaee88dd4e0eb9fefc2032d01
J. Federico Hernandez <fede.hernandez@gmail.com>
2013-03-21 00:13:00 -0300

    Minor fixes

--- deploy-on-dev.sh
+++ deploy-on-dev.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+app_name=$1
+
 # to remove an application
-#adb uninstall nl.psymate
+#adb uninstall $app_name
 
 # to remove the application data
-# adb shell rm -r /data/data/nl.psymate
+# adb shell rm -r /data/data/$app_name
 
 # to install without the need of uninstall before
-adb install -r bin/psymate-mob.apk
+adb install -r bin/${app_name}.apk
+
--- sign-apk.sh
+++ sign-apk.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+app_name=$1
+ver=1.5.4
+stamp=29052012
+
 # Check the apk signature
-#jarsigner -verify -verbose distributions/psymate-mob-v1.5.4_29052012.apk
+#jarsigner -verify -verbose distributions/${app_name}-v${ver}_${stamp}.apk
 
 # Sign the apk
-jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.rdam.keystore distributions/psymate-mob-v1.5.4_29052012.apk rdam
-
-# keystore pass: testito
-# rdam pass: 1d3md1t0
+jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.rdam.keystore distributions/${app_name}-v${ver}_${stamp}.apk rdam
 
 # The SDK tools create the debug keystore/key with predetermined names/passwords:
 # Keystore name: "debug.keystore"
